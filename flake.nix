1aeed167e996f8474747cec49716dbeed84c4830
Drax <drax@nixos>
2026-02-22 19:14:17 +0100

    Nvidia T400 inline flake fixed

--- flake.nix
+++ flake.nix
@@ -13,17 +13,31 @@
       inherit system;
       modules = [
         ./configuration.nix
+        ./hardware-configuration.nix
         ({ config, pkgs, ... }: {
           networking.firewall.enable = false;
           services.openssh.enable = true;
           networking.networkmanager.enable = true;
           security.sudo.wheelNeedsPassword = false;
           users.users.drax.extraGroups = [ "wheel" "video" "input" "networkmanager" ];
+
           services.xserver.enable = true;
           services.xserver.desktopManager.xfce.enable = true;
           services.xserver.displayManager.lightdm.enable = true;
           services.xserver.xkb.layout = "pl";
-          environment.systemPackages = with pkgs; [ git vim firefox xterm wget ];
+
+          # Nvidia T400 Mobile
+          hardware.nvidia = {
+            modesetting.enable = true;
+            powerManagement.enable = false;
+            powerManagement.finegrained = false;
+            open = false;
+            nvidiaSettings = true;
+            package = config.boot.kernelPackages.nvidiaPackages.stable;
+          };
+          services.xserver.videoDrivers = [ "nvidia" ];
+
+          environment.systemPackages = with pkgs; [ git vim firefox xterm wget pciutils nvidia-settings ];
         })
       ];
     };
